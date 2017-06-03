bee883d2b8db3cbe5e56f87f7e376eaf065c7e44
topshik <semenkin_anton@mail.ru>
2017-06-03 17:53:16 +0300

    grid class

---
+++ simulator.cpp
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <vector>
+
+class Cell {
+    double x, y;
+};
+
+class Grid {
+    int length_x, length_y;
+    int n;
+    int population
+    vector<vector<cell>> cells
+
+
+};
+
+void grid_init() {
+
+}
+
+void iteration() {
+
+}
+
+int main() {
+
+}
