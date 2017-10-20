8a238fbb38e78ca568f2080648237c1ed8e908ad
topshik <semenkin.anton@gmail.com>
2017-10-20 15:31:58 +0300

    distance func, get_cell_size() grid method, neighbour_birth_influence() func added but needs fix

--- simulator_d_one.cpp
+++ simulator_d_one.cpp
@@ -9,7 +9,7 @@
 
 const double birth_rate = 0.4;
 const double death_rate = 0.2;
-int wall;
+int wall = 0;
 
 class Cell {
 private:
@@ -57,20 +57,21 @@ public:
 
 class Grid {
 private:
-    unsigned long long population, discretizatiion;
-    double width;
+    unsigned long long population, discretization;
+    double width;  // FIXME: for many dimensions need a vector
     std::vector<Cell> cells;
+    double cell_width;  // FIXME: for many dimensions need a vector
 public:
     Grid() {};
 
-    Grid(unsigned long long Init_population, unsigned long long Discretizatiion, double Width)
-    :population(Init_population), discretizatiion(Discretizatiion), width(Width) {
-        cells = std::vector<Cell>(discretizatiion);
+    Grid(unsigned long long Init_population, unsigned long long Discretization, double Width)
+    :population(Init_population), discretization(Discretization), width(Width) {
+        cells = std::vector<Cell>(discretization);
         Cell init_cell;
         for (unsigned long long i = 0; i != population; ++i) {
             init_cell.set_population(1);
-            init_cell.set_indices(rand() % discretizatiion);             // FIXME: indices, columns, rows??
-            init_cell.set_coordinates((width / discretizatiion) * ((double)init_cell.get_indices() + 0.5));
+            init_cell.set_indices(rand() % discretization);             // FIXME: indices, columns, rows??
+            init_cell.set_coordinates((width / discretization) * ((double)init_cell.get_indices() + 0.5));
             cells[init_cell.get_indices()] = init_cell;
         }
     }
@@ -80,11 +81,15 @@ public:
     }
 
     unsigned long long get_discretization() const {  // FIXME: for many dimensions need a vector
-        return discretizatiion;
+        return discretization;
     }
 
     double get_size() const {  // FIXME: for many dimensions need a vector
         return width;
+    }
+
+    double get_cell_size() const {  // FIXME: for many dimensions need a vector
+        return cell_width;
     }
 
     std::vector<Cell> get_cells() const {  // FIXME: for many dimensions need a vector
@@ -100,9 +105,27 @@ public:
     }
 };
 
+double distance(const Cell &from, const Cell &to) {  // FIXME: for many dimensions need a vector
+    return std::sqrt(std::pow((from.get_coordinates() - to.get_coordinates()), 2));
+}
+
+std::vector<Cell> neighbour_birth_influence(const Grid &grid, const Cell &cell) {
+    // max_distance ? need func for arbitrary Gaussian distribution expectation and variance
+    double max_distance = 0.5;
+    unsigned long long border_x = ceil(max_distance / grid.get_cell_size());  // check for types conversion
+    std::vector<Cell> result;
+    if (wall == 0) {
+        for (unsigned long long i = (cell.get_indices() - border_x < 0 ? 0 : cell.get_indices() - border_x);
+                                i != (cell.get_indices() + border_x >= grid.get_discretization() ? grid.get_discretization() : cell.get_indices() + border_x);
+                                ++i) {
+            result.push_back(grid.get_cells()[i]);
+        }
+    }
+}
+
 int main(int argc, char ** argv) {
     srand(time(NULL));
     Grid grid(10, 10, 1);
-    for (auto cell : grid.get_cells())
+    for (auto cell : neighbour_birth_influence(grid, grid[0]))
         std::cout << cell;
 }
