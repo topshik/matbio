6cad97c940d01d86101f0a1adc04bc0ebbea5b2b
topshik <semenkin.anton@gmail.com>
2018-04-09 13:55:47 +0300

    Update parameters for 1st momentum surface.

--- generate_params.py
+++ generate_params.py
@@ -6,8 +6,8 @@ file.truncate()
 
 file.write("size discretization iterations initial_population birth_rate death_rate birth_variance death_variance seed\n")
 
-for i in np.linspace(1e-5, 1e-3, 50):
-    for j in np.linspace(1e-5, 1e-3, 50):
+for i in np.linspace(1e-5, 1e-3, 30):
+    for j in np.linspace(1e-5, 1e-3, 30):
         file.write("1 5000 5000 300 5e-3 1e-4 " + str(i) + " " + str(j) + "\n")
 
 file.close()
