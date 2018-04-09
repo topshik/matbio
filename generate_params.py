f709881278a4a7cb108083cd1c12c2ac945ebc5f
topshik <semenkin.anton@gmail.com>
2018-04-09 14:36:02 +0300

    Update parameters.

--- generate_params.py
+++ generate_params.py
@@ -6,8 +6,10 @@ file.truncate()
 
 file.write("size discretization iterations initial_population birth_rate death_rate birth_variance death_variance seed\n")
 
+seed = 1
 for i in np.linspace(1e-5, 1e-3, 30):
     for j in np.linspace(1e-5, 1e-3, 30):
-        file.write("1 5000 5000 300 5e-3 1e-4 " + str(i) + " " + str(j) + "\n")
+        file.write("1 5000 5000 300 5e-3 1e-4 " + str(i) + " " + str(j) + " " + str(seed) + "\n")
+        seed += 1
 
 file.close()
